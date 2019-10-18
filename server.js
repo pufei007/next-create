c62da5a5ef985db195bb5e3b48ba5689b014e7e7
pufei <pufei@snqu.com>
2019-10-18 17:32:57 +0800

    3400

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT||3400;
 
 app.prepare().then(() => {
   const server = new Koa();
